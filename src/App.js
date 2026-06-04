2d79d6d6ca69ab6cf3a15fc9f2d826a018528400
V4lar <yassin.trader111@gmail.com>
2026-06-04 23:37:09 +0300

    Comment out App.css import in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "@/App.css";
+// import "@/App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "@/components/ui/sonner";
 import Layout from "@/components/krftist/Layout";
